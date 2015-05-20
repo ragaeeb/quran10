c417c28a9d9c1bf675953c4a8a75440827a8bb6c
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2015-05-20 10:32:13 -0400

    Individual bio page is displaying the target's name instead of the author #374

--- src/QueryHelper.cpp
+++ src/QueryHelper.cpp
@@ -388,7 +388,7 @@ void QueryHelper::fetchBio(QObject* caller, qint64 individualId)
     LOGGER(individualId);
 
     ATTACH_TAFSIR;
-    m_sql.executeQuery(caller, QString("SELECT mentions.id,%1 AS author,heading,body,bio_id,reference,points FROM mentions INNER JOIN biographies ON mentions.bio_id=biographies.id LEFT JOIN individuals i ON target=i.id WHERE target=%2").arg( NAME_FIELD("i") ).arg(individualId), QueryId::FetchBio);
+    m_sql.executeQuery(caller, QString("SELECT mentions.id,%1 AS author,heading,body,bio_id,reference,points FROM mentions INNER JOIN biographies ON mentions.bio_id=biographies.id LEFT JOIN individuals i ON biographies.author=i.id WHERE target=%2").arg( NAME_FIELD("i") ).arg(individualId), QueryId::FetchBio);
 }
 
 
