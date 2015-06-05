7b24140237b9d3793b4315fe982fbde1b0e6eac8
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2015-06-05 10:11:39 -0400

    Fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "precompiled.h"
 
 #include "applicationui.hpp"
+#include "Logger.h"
 
 using namespace bb::cascades;
 using namespace quran;
@@ -8,6 +9,8 @@ using namespace quran;
 Q_DECL_EXPORT int main(int argc, char **argv)
 {
     Application app(argc, argv);
+
+    registerLogging( QString::number( app.applicationPid() ) );
     ApplicationUI ui;
 
     return Application::exec();
